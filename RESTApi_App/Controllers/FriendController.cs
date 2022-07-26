ee2612c8e56e667b1b50680fe0f6ec807aed18a5
Ramesh Sengamalai <rsengamalai@gmail.com>
2022-07-26 19:09:44 +0800

    Initial Commit
    Initial Commit

---
+++ RESTApi_App/Controllers/FriendController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTApi_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FriendController : ControllerBase
+    {
+        public List<Friend> GetFriends()
+        {
+            List<Friend> friends = new List<Friend>(3);
+            friends.Add(new Friend("Bhuvanesh", "Jayaprakash", "Vriddhachalam"));
+            friends.Add(new Friend("Anand", "KS", "Vriddhachalam"));
+            friends.Add(new Friend("Natesan", "K", "Chennai"));
+
+            return friends;
+        }
+    }
+}
---
+++ RESTApi_App/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RESTApi_App
+{
+    public class Friend
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string location { get; set; }
+
+        public Friend()
+        {
+
+        }
+
+        public Friend(string firstName, string lastName, string location)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.location = location;
+        }
+    }
+}
+
